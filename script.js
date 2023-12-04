a449d08c9a70d7d7387e1e3801f39954bb5b9bfb
Julia Gaugler <greengates4j@gmail.com>
2023-12-04 22:04:35 +0200

    Coding Done

--- script.js
+++ script.js
@@ -2,17 +2,8 @@ var question = document.querySelector("details");
 var icon = document.querySelector(".icon");
 var button = document.querySelector("#button");
 
-//question.open;
-
-function changeIcon() {
-
-    if (question.open) {
-        // toggle close
-        question.open = false;
-        icon.src = "assets/images/icon-plus.svg"
-        } else if(question.open = true){
-            icon.src = "assets/images/icon-minus.svg"
-        }
-}
-
-question.addEventListener("click", changeIcon());
+var tie = document.querySelectorAll('.details*:not(:active)');
+console.log(tie);
+tie.addEventListener("click", function(){
+    question.style.color = "red";
+});
