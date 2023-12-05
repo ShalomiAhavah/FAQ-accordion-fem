8bdae788bb6a00f37718b282c22104a8dd951f1d
Julia Gaugler <greengates4j@gmail.com>
2023-12-05 12:16:21 +0200

    Finished Product

--- script.js
+++
@@ -1,9 +0,0 @@
-var question = document.querySelector("details");
-var icon = document.querySelector(".icon");
-var button = document.querySelector("#button");
-
-var tie = document.querySelectorAll('.details*:not(:active)');
-console.log(tie);
-tie.addEventListener("click", function(){
-    question.style.color = "red";
-});
